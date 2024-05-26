443c90d1dc275e3a7c2d1e72ce624ca4d19bab55
Sébastien Herbreteau <seb.eric.herbreteau@gmail.com>
2024-05-26 08:27:57 +0200

    home background ok

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ThumbnailRepository;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(ThumbnailRepository $thumbnailRepository): Response
+    {
+        $thumbnails = $thumbnailRepository->findAll();
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'thumbnails'      => $thumbnails,
+        ]);
+    }
+}
