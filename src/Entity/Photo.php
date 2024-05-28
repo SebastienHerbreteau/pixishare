b78e927bf2ad2b46376f65dca2816c1f280186fc
Sébastien Herbreteau <seb.eric.herbreteau@gmail.com>
2024-05-28 07:37:30 +0200

    Connection system

---
+++ src/Entity/Photo.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PhotoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PhotoRepository::class)]
+class Photo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $file_path = null;
+
+    #[ORM\Column]
+    private ?int $album = null;
+
+    #[ORM\Column]
+    private ?int $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFilePath(): ?string
+    {
+        return $this->file_path;
+    }
+
+    public function setFilePath(string $file_path): static
+    {
+        $this->file_path = $file_path;
+
+        return $this;
+    }
+
+    public function getAlbum(): ?int
+    {
+        return $this->album;
+    }
+
+    public function setAlbum(int $album): static
+    {
+        $this->album = $album;
+
+        return $this;
+    }
+
+    public function getUser(): ?int
+    {
+        return $this->user;
+    }
+
+    public function setUser(int $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
