e2209f7f07871b0c5895e948eb6ff485cf2d960d
Sebastienherbreteau <seb.eric.herbreteau@gmail.com>
2025-01-16 18:10:35 +0100

    Modification progress bar

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -7,15 +7,15 @@ plusButton.addEventListener('click', () => {
     fetch('/gallery/upload')
         .then(res => res.text())
         .then(data => {
+
             let uploadModal = document.querySelector('.upload-modal');
             uploadModal.innerHTML = data;
             uploadModal.style.display = 'block';
             let form = document.querySelector('form');
 
             form.addEventListener('submit', e => {
-                console.log(form);
                 e.preventDefault();
-                let progressContainer = document.querySelector('.container-progress');
+                let progressContainer = document.querySelector('.container');
                 progressContainer.style.display = 'flex';
                 form.style.display = 'none';
                 const formData = new FormData(form);
