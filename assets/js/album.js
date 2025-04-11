845e063a4dcd2b45170bea6c5b5bfa02acc867b4
Sebastienherbreteau <seb.eric.herbreteau@gmail.com>
2025-04-11 11:08:55 +0200

    Modif moda photo

--- assets/js/album.js
+++ assets/js/album.js
@@ -4,10 +4,11 @@ import 'justifiedGallery/dist/js/jquery.justifiedGallery.js';
 let photos = document.querySelectorAll('.photo')
 let modal = document.querySelector('.modal')
 
+
 $('.content').justifiedGallery({
-    rowHeight : 200,
-    lastRow : 'left',
-    margins : 3,
+    rowHeight: 200,
+    lastRow: 'left',
+    margins: 3,
     border: 50,
 });
 
@@ -15,10 +16,18 @@ photos.forEach(photo => {
     photo.addEventListener('click', function () {
         let id = photo.dataset.id
         fetch('/gallery/album/photo/' + id)
-            .then(response => response.text().then(function(content) {
+            .then(response => response.text().then(function (content) {
                 modal.innerHTML = content
                 modal.style.display = 'flex'
-                modal.addEventListener('click', () => modal.style.display = 'none')
+                let mainPhoto = modal.querySelector('#photo')
+                mainPhoto.addEventListener('click', () => modal.style.display = 'none')
+                document.addEventListener('keydown', function (event) {
+                    if (event.key === 'Escape') {
+                        modal.style.display = 'none'
+                    }
+                })
+                console.log(mainPhoto.dataset.photoId)
+
             }))
     })
 })
